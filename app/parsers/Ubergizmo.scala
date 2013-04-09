1125f1eae507e31725539d4d9ae894ce9fb41b19
oxman <informatique@bouh.org>
2013-04-09 15:09:06 +0200

    Ubergizmo wrap video in auto size div.

--- app/parsers/Ubergizmo.scala
+++ app/parsers/Ubergizmo.scala
@@ -6,6 +6,7 @@ import java.util.Locale
 import java.text.SimpleDateFormat
 import org.jsoup.Jsoup
 import org.jsoup.nodes._
+import scala.collection.JavaConversions._
 import scala.xml.Node
 
 class Ubergizmo extends Default {
@@ -15,6 +16,12 @@ class Ubergizmo extends Default {
   override def getContent(item: Node, page: Document) = {
     page.select("span#intelliTxt").first() match {
       case content: Element => {
+        for(video <- content.select("iframe[src*=www.youtube.com]")) {
+          video.removeAttr("height").removeAttr("width")
+          video.removeAttr("class")
+          video.wrap("<div class='videoWrapper'><div></div></div>")
+        }
+
         content.html()
       }
       case null => ""
