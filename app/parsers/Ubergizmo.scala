90203db4591b42b96d5e0ec0f7f58e1e7eab2348
oxman <informatique@bouh.org>
2013-05-16 16:27:36 +0200

    Ubergizmo parse the new cover video for article.

--- app/parsers/Ubergizmo.scala
+++ app/parsers/Ubergizmo.scala
@@ -14,10 +14,20 @@ class Ubergizmo extends Default {
   override def dateFormat = "d MMM yyyy HH:mm:ss Z"
 
   override def getContent(item: Node, page: Document) = {
-    page.select("span#intelliTxt").first() match {
+    val video = page.select(".youtube_subscription_link").first() match {
+      case content: Element => {
+        content.select("p:last-of-type").remove()
+        content.html()
+      }
+      case null => ""
+    }
+
+    val content = page.select("span#intelliTxt").first() match {
       case content: Element => content.html()
       case null => ""
     }
+
+    video + content
   }
 
 }
