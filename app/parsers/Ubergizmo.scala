43e1574ba583c76cbb3a934ca7cad89b06b878c3
oxman <informatique@bouh.org>
2013-06-03 11:33:05 +0200

    update Ubergizmo parser.

--- app/parsers/Ubergizmo.scala
+++ app/parsers/Ubergizmo.scala
@@ -22,8 +22,17 @@ class Ubergizmo extends Default {
       case null => ""
     }
 
-    val content = page.select("span#intelliTxt").first() match {
-      case content: Element => content.html()
+    val content = page.select(".article").first() match {
+      case content: Element => {
+        content.select(".byline_container").remove()
+        content.select("p[data-swiftype-index]").remove()
+        content.select(".social-widgets").remove()
+        content.select(".tag").remove()
+        content.select(".post_taglist").remove()
+        content.select(".indivsection").remove()
+        content.select("#fbfaces").remove()
+        content.html()
+      }
       case null => ""
     }
 
